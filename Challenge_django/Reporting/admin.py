21b866b30d7d36f8eba9cc285b44ca3eca5bd293
Fidel <armelkouakou327@gmail.com>
2020-05-02 17:27:26 +0000

    Enregistrement des models(tables) pour qu'ils apparaissent dans l'interface d'administration de django

--- Challenge_django/Reporting/admin.py
+++ Challenge_django/Reporting/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Categorie_Agent, Agent, Type_Activite, Activite, Fact_Prestation, Periodicite, Periodes_Reporting, Date
 # Register your models here.
+admin.site.register(Categorie_Agent)
+admin.site.register(Agent)
+admin.site.register(Type_Activite)
+admin.site.register(Activite)
+admin.site.register(Fact_Prestation)
+admin.site.register(Periodicite)
+admin.site.register(Periodes_Reporting)
+admin.site.register(Date)
